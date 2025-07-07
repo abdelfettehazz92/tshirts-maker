5c9c2b542551f346d58c5529a219cf70a423970f
abdelfettehazz92 <abdelfettehazzouz838@gmail.com>
2025-07-07 09:20:34 +0100

    Fix AuthProvider wrapper in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
+import { AuthProvider } from './hooks/useAuth'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-    <Toaster 
-      position="top-right"
-      toastOptions={{
-        duration: 4000,
-        style: {
-          background: '#363636',
-          color: '#fff',
-        },
-      }}
-    />
+    <AuthProvider>
+      <App />
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+        }}
+      />
+    </AuthProvider>
   </React.StrictMode>,
-) 
+)
